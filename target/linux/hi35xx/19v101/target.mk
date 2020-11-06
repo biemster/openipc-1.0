8aec346c590bdc06f013e6bc1109f8b58f6ef553
Igor Zalatov <flyrouter@gmail.com>
2020-11-06 18:18:37 +0300

    Add more profiles

--- target/linux/hi35xx/19v101/target.mk
+++ target/linux/hi35xx/19v101/target.mk
@@ -2,13 +2,13 @@
 # Copyright (C) 2012 OpenWrt.org
 #
 
-SUBTARGET:=16av200
-BOARDNAME:=HI3516AV200 (ARMv7) v7l
+SUBTARGET:=19ev101
+BOARDNAME:=HI3519V101 (ARMv7) v7l
 CPU_TYPE:=cortex-a7
 FEATURES+=low_mem
 KERNEL_PATCHVER:=3.18.20
 
 define Target/Description
-	Build firmware images for SoC version kernel 3.18.20. \
+	Build firmware images for SoC version kernel 4.9.37. \
 	Supported hi3516av200|hi3519v101.
 endef
